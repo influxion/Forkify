d09015c805c682656157918dca99544ddf904b26
influxion <nicholsjordon721@gmail.com>
2022-07-04 23:52:48 -0500

    Welcome message added

--- src/js/controller.js
+++ src/js/controller.js
@@ -109,6 +109,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerUpdateServings(controlServings);
@@ -117,6 +121,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
 
