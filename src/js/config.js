0f244303039448b8d0cd1914c7dc3702084dfd48
influxion <nicholsjordon721@gmail.com>
2022-07-05 00:18:48 -0500

    New API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
-export const API_KEY = '10be57fe-24ee-48de-8fdc-eb606b5ca04f';
+export const API_KEY = '0c7a2af6-0a8e-401a-bfd7-65b03bcb4486';
 export const TIMEOUT_SECONDS = 10;
 export const RES_PER_PAGE = 10;
 export const MODAL_CLOSE_SEC = 1;
